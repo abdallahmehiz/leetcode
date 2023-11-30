6bb71b7ec1806822955813b1f389ebc47d46b4d9
Abdallah <54363735+abdallahmehiz@users.noreply.github.com>
2023-11-30 09:09:39 +0100

    Update 0274 - H Index.js

--- problems/0274 - H Index.js
+++ problems/0274 - H Index.js
@@ -5,8 +5,6 @@
 var hIndex = function(citations) {
   citations.sort((a,b) => a - b);
   let result = 0;
-  for(let i = 0; i < citations.length; i++){
-    result = Math.max(result, Math.min(citations[i], citations.length-i));
-  }
+  for(let i = 0; i < citations.length; i++) result = Math.max(result, Math.min(citations[i], citations.length-i));
   return result;
 };
